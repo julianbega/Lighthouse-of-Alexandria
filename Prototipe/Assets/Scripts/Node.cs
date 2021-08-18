2fc391307ce31c3778e85b7f5f40c0a5934cc2e0
Julian Bega <julian.begamilrad@gmail.com>
2021-08-18 09:32:17 -0300

    Change node color with mouse

---
+++ Prototipe/Assets/Scripts/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color selectedColor;
+    private Color startColor;
+    private Renderer render;
+
+    private void Awake()
+    {
+        render = GetComponent<Renderer>();
+        startColor = render.material.color;
+    }
+    private void OnMouseEnter()
+    {
+        render.material.color = selectedColor;
+
+    }
+    private void OnMouseExit()
+    {
+        render.material.color = startColor;
+    }
+}
