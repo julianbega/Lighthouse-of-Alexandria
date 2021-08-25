0310d82f92562628803b1c72042961be23cdeb2c
Julian Bega <julian.begamilrad@gmail.com>
2021-08-25 11:12:52 -0300

    Prototype type1
    beacon light

---
+++ Prototipe/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+    private Camera mainCamera;
+    private Vector3 pos;
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        Ray ray = mainCamera.ScreenPointToRay(mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            pos = new Vector3(raycastHit.point.x, transform.position.y, raycastHit.point.z);
+        }
+        transform.position = Vector3.Lerp(transform.position, pos, moveSpeed);
+    }
+}
