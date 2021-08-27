7993387c6a6ab44c56e06047876f4596380488c9
Julian Bega <julian.begamilrad@gmail.com>
2021-08-27 20:15:27 -0300

    Cheats + WaveSpawnerProto3

--- Prototipe/Assets/Scripts/Cheats.cs
+++ Prototipe/Assets/Scripts/Cheats.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Cheats : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameManager gm;
+
+    public int MoneyToAdd;
+    public int LivesToAdd;
     void Start()
     {
-        
+        gm = GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            gm.money += MoneyToAdd;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            gm.money = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            gm.lives += LivesToAdd;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            gm.lives -= 1;
+        }
     }
 }
---
+++ Prototipe/Assets/Scripts/WaveSpawnerProto3.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawnerProto3 : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform[] spawnStart;
+
+    public float timeBetweenWaves;
+    public int maxCantOfEnemiesPerWave;
+    public bool waveIsInCourse;
+    private float timerContdown = 2.0f;
+    private int waveCount = 0;
+    private int enemyCount;
+
+    private bool startWave;
+
+    private void Start()
+    {
+        WaveManager.StartWaveEvent += StartWaveCycle;
+        Enemy.EnemyDie += DecreaseEnemyCount;
+        FreeEnemy.EnemyDie += DecreaseEnemyCount;
+        enemyCount = 0;
+        startWave = false;
+    }
+
+    private void Update()
+    {
+        if (startWave && enemyCount <= 0 && waveCount <= 7)
+        {
+            StartCoroutine(SpawnWave());
+        }
+        Debug.Log("enemyCount: " + enemyCount);
+        Debug.Log("waves: " + waveCount);
+    }
+
+    void StartWaveCycle()
+    {
+        startWave = true;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < maxCantOfEnemiesPerWave; i++)
+        {
+            if (enemyCount < maxCantOfEnemiesPerWave) //maximo 7 oleadas
+            {
+                SpawnEnemy();
+                enemyCount++;
+                yield return new WaitForSeconds(0.25f);
+            }
+        }
+        waveCount++;
+    }
+
+    void SpawnEnemy()
+    {
+        int spawner = Random.Range(0, spawnStart.Length);
+        Instantiate(enemyPrefab, spawnStart[spawner].transform, true);
+    }
+
+    void DecreaseEnemyCount()
+    {
+        if (enemyCount >= 0)
+            enemyCount--;
+    }
+
+    public int GetWaveCount()
+    {
+        return waveCount;
+    }
+
+    private void OnDisable()
+    {
+        WaveManager.StartWaveEvent -= StartWaveCycle;
+        Enemy.EnemyDie -= DecreaseEnemyCount;
+        FreeEnemy.EnemyDie -= DecreaseEnemyCount;
+    }
+}
