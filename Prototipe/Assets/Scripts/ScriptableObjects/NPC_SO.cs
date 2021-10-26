75080aad3ff35cb377a90a672d863bcc4008f7fe
Julian Bega <julian.begamilrad@gmail.com>
2021-10-26 20:47:42 -0300

    NPC scriptable objects

---
+++ Prototipe/Assets/Scripts/ScriptableObjects/NPC_SO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC", menuName = "ScriptableObjects/NPCScriptableObject", order = 1)]
+public class NPC_SO : ScriptableObject
+{
+    public string NPC_name;
+    public Sprite image;
+    public Sprite background;
+}
