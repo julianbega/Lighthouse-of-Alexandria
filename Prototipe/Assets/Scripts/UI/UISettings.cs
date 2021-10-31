dc85f0da0879be00f077324b5ca3a98594092842
Elias Brucart <eliasbrucart@hardgames.com.ar>
2021-10-31 02:50:17 -0300

    Added settings screen in Gameplay

---
+++ Prototipe/Assets/Scripts/UI/UISettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISettings : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsPanel;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ActivateSettingsPanel()
+    {
+        settingsPanel.gameObject.SetActive(true);
+    }
+
+    public void CloseSettingsPanel()
+    {
+        settingsPanel.gameObject.SetActive(false);
+    }
+}
