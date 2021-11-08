1492cd282894a43e5317391523671f529440a45a
Julian Bega <julian.begamilrad@gmail.com>
2021-11-07 21:20:40 -0300

    InvestigationScriptableObjects

---
+++ Prototipe/Assets/Scripts/ScriptableObjects/Investigation_SO.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Investigation", menuName = "ScriptableObjects/InvestigationScriptableObject", order = 1)]
+public class Investigation_SO : ScriptableObject
+{
+    public string name;
+    public Sprite image;
+    public Sprite imageLedge;
+    public string description;
+    public int price;
+    public int timeInDays;
+    public bool AllreadyInvestigated;
+    public Investigation_SO previousInvestigation;
+}
